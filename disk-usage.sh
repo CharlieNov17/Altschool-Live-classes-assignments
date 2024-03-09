89b263f26cf525b0ed9df03186c544d4d3aebb35
Chibuzo Nwobia <chibuzo.nwobia@gmail.com>
2024-03-09 15:05:29 +0100

    Update disk-usage.sh

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-#du -d 5 -h /etc | sort -hr | head -5
-#diskusage.sh -n /etc
-
-directory=$@
-number_of_files=$(ls -l "$directory" | grep -c "^")
-
 if [[ $1 == '-d' ]]; then
-  dir_files=true
-  shift 1
+
+all_files=true
+shift 1
+
 fi
 
-if [[ $1 == '-n' && $2=~^[0-9]+$ ]]; then
-  entries=$2
-  shift 2
+ #du -h  /etc | sort -rh | head -5
+
+#diskusage.sh -n 5 /etc
+if [[ $1 == '-n' && $2=~^[0-9]+$ ]]; then 
+
+    entries=$2
+    shift 2
 else
-  entries=8
+    entries=8
+fi
+directory=$1
+
+if [[ $all_files = true ]]; then
+
+sudo du -h  $1 | sort -rh | head -$entries
+
+else
+
+sudo du -h --max-depth=1 $1 | sort -rh | head -$entries
 fi
 
-list_disk_usage() {
-}
+#du -h  $3 | sort -rh | head -$entries
 
 
 
-
-for dir in $directory; do
-  echo "Disk Usage for directory: $dir"
-  list_disk_usage $dir
-done
-
-echo " "
-echo "The total number of files in '$directory' = $number_of_files"
-
-# echo "$1 - $2 - $3"
+#diskusage.sh /etc
